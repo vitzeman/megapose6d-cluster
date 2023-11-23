de3ddc698566123d3837d9a6f3381572ad319d9a
Vit Zeman <vit.zeman.cz@gmail.com>
2023-11-23 12:56:49 +0000

    Print settings visualization update

--- Server.py
+++ Server.py
@@ -23,7 +23,18 @@ K_PATH = Path(
 )
 # cv2.namedWindow("TestServer", cv2.WINDOW_NORMAL)
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+
+LABELS = {
+    1: "d01_controller",
+    2: "d02_servo",
+    3: "d03_main",
+    4: "d04_motor",
+    5: "d05_axle_front",
+    6: "d06_battery",
+    7: "d07_axle_rear",
+    8: "d08_chassis",
+}
 
 
 def parse_args():
@@ -63,6 +74,7 @@ def run_pose_est_server():
         with conn:
             print(f"connection established from: {address}")
             while True:
+                print("Waiting for data")
                 img = conn.recv(1024)
                 bbox = conn.recv(1024)
                 label = conn.recv(1024)
@@ -71,22 +83,25 @@ def run_pose_est_server():
                 if idx == -1:  # TODO: Maybe finish when label -1 was sent
                     break
 
-                print(f"Data recieved: img {img.shape}, bbox = {bbox}, label = {label}")
                 # Sent back the data
                 # TODO: Add function to process the data and get the pose(quaternion + translation)
                 # TODO: Bassically write in megapose function to get the pose
-                print(f"Running inference")
+                print(f"Running inference on incoming data:")
+                print(f"\timg {img.shape}, bbox = {bbox}, label = {idx}:={LABELS[idx]}")
                 pose = pose_estimator.run_inference(img, bbox, label)
-
-                if visualize:
-                    print(f"Running Visualiztion")
-                    pose_estimator.visualize_pose(pose, img, label)
 
                 # pose = np.array([1, 2, 3, 4, 5, 6, 7])  # quaternion + translation
                 conn.send(pose)
 
                 # NOTE: Maybe add some rendering to the background for the
-                print(f"Send result {pose}")
+                print(
+                    f"Sent result (quat [:4] translation [4:])\n \tquat{pose[:4]}\n\ttrnl{pose[4:]}"
+                )
+
+                if visualize:
+                    print(f"Running Visualiztion")
+                    pose_estimator.visualize_pose(pose, img, label)
+
                 # break  # TODO: Remove after testing for iterations
     print("Closing server")
 
